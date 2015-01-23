4713ee016f10e7760f1b4fbf5ac295852028a17e
Kyle Isom <kyle@cloudflare.com>
2015-01-22 20:23:48 -0800

    Initial import.

---
+++ .mg
@@ -0,0 +1,4 @@
+auto-indent-mode
+dired-backup-unflag
+make-backup-files 
+set-fill-column 72
---
+++ config.h
@@ -0,0 +1,10 @@
+#define FKEYS
+#define REGEX
+#define XKEYS
+#ifndef LOGIN_NAME_MAX
+#define MAXLOGNAME _POSIX_LOGIN_NAME_MAX
+#endif
+#ifndef MAXLOGNAME
+#define MAXLOGNAME LOGIN_NAME_MAX
+#endif
+#define HAVE_ARC4RANDOM
